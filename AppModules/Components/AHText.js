b7b1806a21c6bd7922b08742b4ff1b99b0b077cd
lakshu97 <lakshminarasimhan@eshakti.com>
2024-02-01 22:00:35 +0530

    ProductDetail.js done half

--- AppModules/Components/AHText.js
+++ AppModules/Components/AHText.js
@@ -1,4 +1,12 @@
 import React from 'react';
-import {Text} from 'react-native-paper';
+import {StyleSheet} from "react-native";
+import {MD2Colors, Text} from 'react-native-paper';
 const AHText = props => <Text {...props}>{props.name}</Text>;
+export const AHWhiteText = props => (
+  <Text
+    style={StyleSheet.compose(props?.style, {color: MD2Colors.white})}
+    {...props}>
+    {props.name}
+  </Text>
+);
 export default React.memo(AHText);
--- AppModules/Screens/ProductDetail/Views/ProductDetail.js
+++ AppModules/Screens/ProductDetail/Views/ProductDetail.js
@@ -1,18 +1,68 @@
 import React from 'react';
 import {HeaderComponent} from '../../../Components/HeaderComponent';
-import {View} from 'react-native';
+import {Dimensions, ScrollView, View} from 'react-native';
 import productStyles from '../styles/ProductDetailStyles';
-import {useRoute} from "@react-navigation/native";
-import {Text} from "react-native-paper";
+import {useRoute} from '@react-navigation/native';
+import {MD2Colors, Text} from 'react-native-paper';
+import {AHWhiteText} from '../../../Components/AHText';
+import FastImage from 'react-native-fast-image';
+const height = Dimensions.get('window').height;
 const ProductDetail = () => {
-    const route = useRoute();
+  const route = useRoute();
+  let item = route.params?.item;
 
   return (
     <View style={productStyles.container}>
-      <HeaderComponent />
-        <Text>{route.params?.item?.name}</Text>
-
+      <HeaderComponent showHeader={true} name={'Product Detail'} />
+      <FastImage
+        style={{
+          width: '95%',
+          height: height / 3,
+          alignSelf: 'center',
+          padding: 5,
+        }}
+        resizeMode={FastImage.resizeMode.cover}
+        source={{
+          uri: item.image,
+          priority: FastImage.priority.normal,
+        }}
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginVertical: 10,
+          padding: 1,
+          alignItems: 'center',
+          alignContent: 'center',
+          flex: 0.2,
+        }}>
+        <Text
+          style={{
+            flex: 0.5,
+            color: MD2Colors.white,
+            fontSize: 22,
+            marginHorizontal: 10,
+            marginVertical: 5,
+            padding: 6,
+          }}
+          variant={'bodyMedium'}>
+          {item.name}
+        </Text>
+        <Text
+          style={{
+            color: MD2Colors.white,
+            textAlign: 'center',
+            marginVertical: 5,
+            flex: 0.5,
+            alignSelf: 'center',
+            marginHorizontal: 10,
+          }}
+          variant={'bodyLarge'}>
+          $ {item.price}
+        </Text>
+      </View>
     </View>
   );
 };
-export default React.memo(ProductDetail)
+export default React.memo(ProductDetail);
